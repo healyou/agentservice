3b38367c3f36b9a2a16a7141d0e86b5195acf251
Nikita Gorodilov <healyou1994@gmail.com>
2017-09-22 21:43:17 +0300

    фикс тестовых данных

--- db/src/sql/create_test_data.sql
+++ db/src/sql/create_test_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO agent (mas_id, name, type_id) VALUES ('test1', 'test1', 1);
+INSERT INTO agent (mas_id, name, type_id) VALUES ('test1', 'test1', 2);
 INSERT INTO agent (mas_id, name, type_id) VALUES ('test2', 'test2', 1);
 INSERT INTO agent (mas_id, name, type_id) VALUES ('masId', 'name', 1);
 
