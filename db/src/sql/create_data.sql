eafc32263721e91ecdf573aa50113c482e51bc15
Nikita Gorodilov <healyou1994@gmail.com>
2017-12-19 17:23:51 +0300

    тестовые данные

--- db/src/sql/create_data.sql
+++ db/src/sql/create_data.sql
@@ -1,27 +1,21 @@
 ------------------ agentType data ------------------
 INSERT INTO agent_type (code, name) VALUES
-    ('worker', 'Рабочий агент'),
-    ('server', 'Серверный агент'),
-    ('test_agent_type_1', 'Тип тестового агента 1'),--integration test
-    ('test_agent_type_2', 'Тип тестового агента 2');--integration test
+    ('integration_test_agent_type_1', 'Тип тестового агента 1(Интеграционные тесты)'),
+    ('integration_test_agent_type_2', 'Тип тестового агента 2(Интеграционные тесты)');
 
 -------------- integration test data - agent --------------
 INSERT INTO agent (mas_id, name, type_id) VALUES
-    ('test_agent_1_masId', 'Тестовый агент 1', 3),--integration test
-    ('test_agent_2_masId', 'Тестовый агент 2', 4),--integration test
-    ('masId', 'masId', 1);
+    ('integration_test_agent_1_masId', 'Тестовый агент 1(Интеграционные тесты)', 1),
+    ('integration_test_agent_2_masId', 'Тестовый агент 2(Интеграционные тесты)', 2);
 
 ------------------ communicationGoal data ------------------
 INSERT INTO message_goal_type (code, name) VALUES
-    ('task_decision', 'Решение задачи'),
-    ('test_message_goal_type_1', 'Тестовая цель общения 1');
+    ('integration_test_message_goal_type_1', 'Тестовая цель общения 1(Интеграционные тесты)');
 
 ------------------ messageType data ------------------
 INSERT INTO message_type (code, name, message_order, message_goal_type_id) VALUES
-    ('search_task_solution', 'Поиск решения задачи', 1, 1),
-    ('search_solution', 'Поиск решения', 2, 1),
-    ('solution_answer', 'Ответ на запрос решения задачи', 3, 1),
-    ('task_solution_answer', 'Ответ на задачу', 4, 1);
+    ('integration_test_message_type_1_test_goal_1', 'Тестовый тип сообщения 1 для тестовой цели 1(Интеграционные тесты)', 1, 1),
+    ('integration_test_message_type_2_test_goal_1', 'Тестовый тип сообщения 2 для тестовой цели 1(Интеграционные тесты)', 2, 1);
 
 ------------------ messageBodyType data ------------------
 INSERT INTO message_body_type (code, name) VALUES
